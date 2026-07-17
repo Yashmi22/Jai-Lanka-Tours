202bbfe2dcbffd92b7e3f4f230e271a6b80832b2
Yashmi22 <nirashayashmi@gmail.com>
2026-07-17 23:57:08 +0530

    packagejson edit

--- server/server.js
+++ server/server.js
@@ -87,6 +87,7 @@ app.post('/api/admin/login', async (req, res) => {
 
 // --- SERVER STARTUP & DATABASE CONNECTION ---
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 mongoose.connect(process.env.MONGO_URI)
