54713169dbd17872b93dd6a73dc4a487f74d5511
Yasith Nawanjana <149180967+NawanjanaYasith@users.noreply.github.com>
2026-08-07 23:36:36 +0530

    fix issues

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // server local or railway
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 
